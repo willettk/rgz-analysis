55e90a4378b0aa30a3b566af38115002b1eb81b9
Kyle Willett <willettk@gmail.com>
2014-06-13 16:12:45 -0500

    Counting number of IR peaks in expert sample

--- python/rgz.py
+++ python/rgz.py
@@ -1,7 +1,7 @@
 # import necessary python packages
 import numpy as np
 import datetime
-import os
+import os,sys
 import urllib
 import cStringIO
 import json
@@ -10,10 +10,12 @@ from scipy.ndimage.filters import maximum_filter
 from scipy.ndimage.morphology import generate_binary_structure, binary_erosion
 from dateutil.parser import parse
 from matplotlib import pyplot as plt
+from matplotlib import cm
 from collections import Counter
 from pymongo import MongoClient
 from scipy import stats
 from PIL import Image
+from collections import OrderedDict
 
 #------------------------------------------------------------------------------------------------------------
 
@@ -60,11 +62,21 @@ def make_subdirs(expert_user):
 
     return None
 
+def list_flatten(x):
+    result = []
+    for el in x:
+        if hasattr(el, "__iter__") and not isinstance(el, basestring):
+            result.extend(list_flatten(el))
+        else:
+            result.append(el)
+
+    return result
+
 def plot_npeaks(expert_user):
 
     # Read in data
 
-    with open('%s/npeaks_ir_expert_%s.csv' % (csv_dir,expert_user),'rb') as f:
+    with open('%s/expert/npeaks_ir_expert_%s.csv' % (csv_dir,expert_user),'rb') as f:
         npeaks = [int(line.rstrip()) for line in f]
     
     # Plot the distribution of the total number of IR sources per image
@@ -214,7 +226,7 @@ def find_consensus(sub,classifications,expert_user,expert_dates,verbose=False):
     # loop over the number of classifications
     if Nusers is not None:			# the number of classifications should equal the number of users who classified
 
-        classfile2 = open('%s/%s/RGZBETA2-%s-classifications.txt' % (dat_dir,expert_user,srcid), 'w')
+        classfile2 = open('%s/%s/RGZBETA2-%s-classifications.txt' % (dat_dir,expert_user,srcid), 'w') # Really should remove the BETA2 designation
     
         # initialise coordinate variables
         radio_ra = []
@@ -405,7 +417,7 @@ def run_expert_sample(subjects,classifications,expert_user,expert_dates):
     expert_zid = open('%s/expert/expert_all_zooniverse_ids.txt' % rgz_dir).read().splitlines()
 
     N = 0
-    with open('%s/npeaks_ir_expert_%s.csv' % (csv_dir,expert_user),'wb') as f:
+    with open('%s/expert/npeaks_ir_expert_%s.csv' % (csv_dir,expert_user),'wb') as f:
         for sub in list(subjects.find({'zooniverse_id':{'$in':expert_zid},'classification_count':{'$gt':0}})):
         
             Nclass = sub["classification_count"]	# number of classifications made per image
@@ -431,18 +443,166 @@ Find a way to automatically check whether classifications matched for a particul
 Maybe by reading the .dat files?
 
 ########################################
+'''
+def sdi(data):
+
+    # Shannon diversity index
+    
+    def p(n, N):
+        """ Relative abundance """
+        if n is 0:
+            return 0
+        else:
+            return (float(n)/N) * np.log(float(n)/N)
+            
+    N = sum(data.values())
+    
+    return -sum(p(n, N) for n in data.values() if n is not 0)
 
 def compare_expert_consensus():
 
+    with open('%s/expert/expert_all_first_ids.txt' % rgz_dir,'rb') as f:
+        first_ids = [line.rstrip() for line in f]
 
-'''
+    exlist = load_expert_parameters()
 
-# If program is called from the command line, process the full dataset
+    ir_array = []
+    ex_array = []
+    for first in first_ids:
+        ir_temp = []
+        exlist_arr = []
+        for ex in exlist:
+            with open('%s/datfiles/expert/%s/RGZBETA2-%s-classifications.txt' % (rgz_dir,ex['expert_user'],first)) as f:
+                x = [line.rstrip() for line in f]
+                try:
+                    last_line = x[-1].split()
+                    n_ir = int(last_line[1])
+                    x_ir = last_line[3]
+                    if x_ir == '-99.0':
+                        n_ir = 0
+                    ir_temp.append(n_ir)
+                    exlist_arr.append(ex['expert_user'])
+                except:
+                    pass
+        
+        ex_array.append(exlist_arr)
+        ir_array.append(ir_temp)
+
+    '''
+    # Plot number of users per galaxy
+
+    excount = [len(x) for x in ex_array]
+    fig2 = plt.figure(2)
+    ax2 = fig2.add_subplot(111)
+    ax2.hist(excount,bins=6,range=(5,11))
+    ax2.set_xlim(6,11)
+    fig2.show()
+    fig2.tight_layout()
+    '''
+
+    c = [Counter(i) for i in ir_array]
+
+    fig = plt.figure(1,(15,4))
+    ax = fig.add_subplot(111)
+
+    larr = []
+    varr = []
+    sdi_arr = []
+    for idx,cc in enumerate(c):
+        l,v = zip(*cc.items())
+        larr.append(list(l))
+        varr.append(list(v))
+        if len(l) > 1:
+            sdi_arr.append(sdi(cc)/np.log(len(l)))
+        else:
+            sdi_arr.append(sdi(cc))
+
+    iarr = []
+    sarr = []
+    for idx,(l,s) in enumerate(zip(larr,sdi_arr)):
+        iarr.append(np.zeros(len(l),dtype=int)+idx)
+        sarr.append(np.zeros(len(l),dtype=float)+s)
+
+    iarr = list_flatten(iarr)
+    larr = list_flatten(larr)
+    varr = list_flatten(varr)
+    sarr = list_flatten(sarr)
+
+    zipped = zip(sarr,larr,iarr,varr)
+    zipped.sort()
+    sarr,larr,iarr,varr = zip(*zipped)
+
+    ikeys = list(OrderedDict.fromkeys(iarr))
+    inew = [ikeys.index(ii) for ii in iarr]
+
+    sc = ax.scatter(inew,larr,c=sarr,s=((np.array(varr)+5)**2),cmap = cm.RdBu_r,vmin=0.,vmax =1.0)
+    cbar = plt.colorbar(sc)
+    cbar.set_label('Normalized Shannon entropy')
+
+    ax.set_xlim(-1,101)
+    ax.set_xlabel('Galaxy')
+    ax.set_ylabel('Number of IR sources')
+    ax.set_aspect('auto')
+
+    lnp = np.array(larr)
+    inp = np.array(inew)
+    for i in inp:
+        if (inp == i).sum() > 1:
+            lmin = np.min(lnp[inp == i])
+            lmax = np.max(lnp[inp == i])
+
+            ax.plot([i,i],[lmin,lmax],color='black')
+
+    fig.show()
+    fig.tight_layout()
+
+
+    
+    '''
+    OK. So, Kevin doesn't have any classifications in the set recorded. There are 134 classifications by his IP address in the timeframe
+    in question, but they're very short (only 18 minutes), and none of them are in the set of 100. Lots of duplicates, too.
+
+    Seven users classified every galaxy in the sample of 100. 
+    42jkb, ivywong, enno.middelberg, xDocR, KWillett, stasmanian, akpinska
+
+    klmasters only shows up for 25 of them (as expected)        Galaxies done in timeframe:
+    vrooje is only in 74 (rest are blank)                       109                                 - Missed last set - asked her to redo it
+    Kevin appears in 0                                          139                                   - Missed all of them - asked him what happened
+
+
+    '''
+
+    
+
+
+
+
+
+
+
+
+    return None
+
+########################################
+# Call program from the command line
+########################################
 
 if __name__ == '__main__':
 
     subjects,classifications,users = load_rgz_data()
-    experts = load_expert_parameters()
+
+    # If arguments are given from command line, process only those users
+    # Note that MongoDB and Python are case-sensitive!! (eg, "Stasmanian" != "stasmanian")
+    if len(sys.argv) > 1:
+        exlist = load_expert_parameters()
+        experts = []
+        for ex in exlist:
+            if ex['expert_user'] in sys.argv[1:]:
+                experts.append(ex)
+    # Otherwise, run it for all users in the parameter file
+    else:
+        experts = load_expert_parameters()
+
     for ex in experts:
 
         expert_user = ex['expert_user']
