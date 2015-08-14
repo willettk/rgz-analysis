300ba36874839f77edac975532af8871614bcee4
Kyle Willett <willettk@gmail.com>
2015-08-13 22:45:06 -0500

    Fixing mistakes

--- python/static_catalog.py
+++ python/static_catalog.py
@@ -12,14 +12,15 @@ def load_data():
     return catalog
 
 def run_static(catalog):
-    filename = '%s/csv/static_catalog.csv'
+    filename = '%s/csv/static_catalog.csv' % path
     with open(filename,'w') as f:
 
         # Header
         print >> f,'source_id peak1_ra peak1_dec peak1_flux peak2_ra peak2_dec peak2_flux wise_ra wise_dec wise_w1mag'
         # Data requested by Larry for double-peaked sources
         for c in catalog.find({'radio.numberComponents':2,'AllWISE':{'$exists':True}}):
-            print >> f,'RGZ_%i' % c['catalog_id'],c['radio']['peaks'][0]['ra'],c['radio']['peaks'][0]['dec'],c['radio']['peaks'][0]['flux'],c['radio']['peaks'][1]['ra'],c['radio']['peaks'][1]['dec'],c['radio']['peaks'][1]['flux'],c['AllWISE']['ra'],c['AllWISE']['dec'],c['AllWISE']['w1mpro']
+            if c['AllWISE'] != None:
+                print >> f,'RGZ_{0:} {1:.3f} {2:.3f} {3:.2f} {4:.3f} {5:.3f} {6:.2f} {7:.3f} {8:.3f} {9:.2f}'.format(c['catalog_id'],c['radio']['peaks'][0]['ra'],c['radio']['peaks'][0]['dec'],c['radio']['peaks'][0]['flux'],c['radio']['peaks'][1]['ra'],c['radio']['peaks'][1]['dec'],c['radio']['peaks'][1]['flux'],c['AllWISE']['ra'],c['AllWISE']['dec'],c['AllWISE']['w1mpro'])
 
 if __name__ == "__main__":
     catalog = load_data()
